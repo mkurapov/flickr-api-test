5ca79d3aca8b3a4e1db9ac63f995b8726fee1411
mkurapov <maxkurapov@gmail.com>
2016-05-20 22:25:01 -0600

    properly created array of photos, which are passed to template main

--- angular2/app/app.component.ts
+++ angular2/app/app.component.ts
@@ -4,7 +4,7 @@ import 'rxjs/Rx';
 
 @Component({
     selector: 'my-app',
-    inputs: ['url', 'buttonActivated', 'photoUrl'],
+    inputs: ['url', 'buttonActivated', 'testUrl'],
     templateUrl:'./templates/main.html'
 
 })
@@ -16,16 +16,16 @@ export class AppComponent {
     url:string = "";
     buttonActivated:boolean = false;
     photos:Array<Photo>=[];
-    photoUrl:string;
+    testUrl:string = "";
 
 
     constructor(public http: Http){}
 
     searchPhoto(){
 
-            var tag = 'blue';
+            var tag = 'purple';
             var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.API_KEY + '&tags=' + tag + '&format=json&nojsoncallback=1';
-            console.log(url);
+            //console.log(url);
             this.url = url;
             this.buttonActivated = true;
             this.http.get(url).map(res => res.json()).subscribe(
@@ -34,22 +34,31 @@ export class AppComponent {
                     //console.log(this.photoUrl);
                     this.photos = [];
 
-                    for(var i in data.photos)
+                    for(var i in data.photos.photo)
                     {
-                      this.photos.push(new Photo(data.photos[i].id, data.photos[i].server, data.photos[i].farm, data.photos[i].secret));
+                      var newPhoto = data.photos.photo[i];
+
+                      this.photos.push(new Photo(newPhoto.id, newPhoto.server, newPhoto.farm, newPhoto.secret));
+
                     }
+
                 },
                 err => {console.log (err)}
                 );
 
-
      }
+
 
 }
 
+
+
 class Photo
 {
-    constructor(public id:string,public server:string,public farm:string,public secret:string){
+    url:string = "";
 
+    constructor(public id:string,public server:string,public farm:string,public secret:string){
+        this.url = 'https://farm'+this.farm+'.staticflickr.com/'+this.server+'/'+this.id+'_'+this.secret+'.jpg';
     }
+
 }
